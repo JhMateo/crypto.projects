a3d6e2ab9f67fed7e75c2ca7cb3f7258e660b246
Jh Mateo <jmgranada@unillanos.edu.co>
2024-04-05 15:11:49 -0500

    Everything is divided into functions to improve the readability and modularity of the code.

--- Sign.cs
+++ Sign.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
-using System.Linq;
-using System.Runtime.Intrinsics.X86;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace crypto.projects
 {
@@ -16,119 +14,143 @@ namespace crypto.projects
 
         public void SignMessage(Keys keys)
         {
-            // Check if keys.json exists
-            if (!File.Exists("keys.json"))
-            {
-                Console.WriteLine("Error: No se encontraron pares de claves. Por favor genera un par primero.");
+            if (!CheckKeyExistence(keys))
                 return;
-            }
 
-            // Load key pairs from JSON
-            keys.LoadFromJson();
+            DisplayPublicKeys(keys);
 
-            if (keys.keyPairs.Count == 0)
-            {
-                Console.WriteLine("Error: No se encontraron pares de claves. Por favor genera un par primero.");
+            int keyIndex = ChoosePublicKey(keys);
+            if (keyIndex == -1)
                 return;
-            }
-
-            Console.WriteLine("Llaves públicas disponibles:");
-            int i = 0;
-            foreach (KeyPair keyPair in keys.keyPairs)
-            {
-                Console.WriteLine($"{i}. {keyPair.PublicKey}");
-                i++;
-            }
-
-            // Elegir llave publica
-            Console.WriteLine("\n > Elija una llave publica para firmar el mensaje (Ingrese el número de la llave):");
-            string keyChoice = Console.ReadLine();
-
-            int keyIndex;
-            if (!int.TryParse(keyChoice, out keyIndex) || keyIndex < 0 || keyIndex > (keys.keyPairs.Count - 1))
-            {
-                Console.WriteLine("Error: Selección de clave no válida. Por favor elige un número válido.");
-                return;
-            }
 
             KeyPair selectedKeyPair = keys.keyPairs[keyIndex];
-            Console.WriteLine(selectedKeyPair.PublicKey);
 
             Console.WriteLine("\n > Escriba el mensaje para firmar:");
             string message = Console.ReadLine();
 
-            // Firmar el archivo de texto
-            if (message != null)
+            if (string.IsNullOrWhiteSpace(message))
             {
-                byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
-                string publicKey = selectedKeyPair.PublicKey;
-
-                RSAParameters privateKeyParams = new RSAParameters
-                {
-                    D = selectedKeyPair.Parameters["D"],
-                    P = selectedKeyPair.Parameters["P"],
-                    Q = selectedKeyPair.Parameters["Q"],
-                    Modulus = selectedKeyPair.Parameters["Modulus"],
-                    DQ = selectedKeyPair.Parameters["DQ"],
-                    DP = selectedKeyPair.Parameters["DP"],
-                    Exponent = selectedKeyPair.Parameters["Exponent"],
-                    InverseQ = selectedKeyPair.Parameters["InverseQ"]
-                };
-
-                try
-                {
-                    using (RSACryptoServiceProvider rsa2 = new RSACryptoServiceProvider())
-                    {
-                        rsa2.ImportParameters(privateKeyParams);
-                        byte[] sign = rsa2.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-                        // Guardar la firma, el mensaje y la llave publica en un archivo
-                        File.WriteAllBytes("sign.txt", sign);
-                        File.WriteAllText("message.txt", message);
-                        File.WriteAllText("publicKey.txt", publicKey);
-
-                        // Crear un nombre de archivo ZIP único
-                        string zipFileName = $"firma_{zipCounter}.zip";
-
-                        // Crear un archivo ZIP para comprimir los archivos
-                        using (ZipArchive zip = ZipFile.Open(zipFileName, ZipArchiveMode.Create))
-                        {
-                            // Agregar el archivo de firma
-                            zip.CreateEntryFromFile("sign.txt", "sign.txt");
-
-                            // Agregar el archivo de mensaje
-                            zip.CreateEntryFromFile("message.txt", "message.txt");
-
-                            // Agregar el archivo de clave pública
-                            zip.CreateEntryFromFile("publicKey.txt", "publicKey.txt");
-                        }
-
-                        // Eliminar los archivos temporales
-                        File.Delete("sign.txt");
-                        File.Delete("message.txt");
-                        File.Delete("publicKey.txt");
-
-                        Console.WriteLine($"\nFirma, mensaje y llave publica comprimidos en {zipFileName}\n");
-                    }
-                }
-                catch (CryptographicException ex)
-                {
-                    if (ex.Message.Contains("The key set does not exist"))
-                    {
-                        Console.WriteLine("Error: El conjunto de claves no existe. Verifique la importación de la clave pública.");
-                        // Registrar el error con más detalles
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error al firmar el mensaje: " + ex.Message);
-                        // Registrar el error con más detalles
-                    }
-                }
-
-            } else
-            {
-                Console.WriteLine("No se recibió ningún mensaje...");
+                Console.WriteLine("Error: No se ingresó ningún mensaje.\n");
+                return;
             }
+
+            try
+            {
+                byte[] signature = GenerateSignature(selectedKeyPair, message);
+
+                if (signature == null)
+                    return;
+
+                CompressAndSaveFiles(signature, message, selectedKeyPair.PublicKey);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al firmar el mensaje: {ex.Message}\n");
+                // Registrar el error con más detalles
+            }
+        }
+
+        private bool CheckKeyExistence(Keys keys)
+        {
+            if (!File.Exists("keys.json") || keys.keyPairs.Count == 0)
+            {
+                Console.WriteLine("Error: No se encontraron pares de claves. Por favor genera un par primero.\n");
+                return false;
+            }
+            return true;
+        }
+
+        private void DisplayPublicKeys(Keys keys)
+        {
+            Console.WriteLine("Llaves públicas disponibles:");
+            for (int i = 0; i < keys.keyPairs.Count; i++)
+            {
+                Console.WriteLine($"{i}. {keys.keyPairs[i].PublicKey}");
+            }
+        }
+
+        private int ChoosePublicKey(Keys keys)
+        {
+            Console.WriteLine("\n > Elija una llave pública para firmar el mensaje (Ingrese el número de la llave):");
+            string keyChoice = Console.ReadLine();
+
+            if (!int.TryParse(keyChoice, out int keyIndex) || keyIndex < 0 || keyIndex >= keys.keyPairs.Count)
+            {
+                Console.WriteLine("Error: Selección de clave no válida. Por favor elige un número válido.\n");
+                return -1;
+            }
+
+            return keyIndex;
+        }
+
+        private byte[] GenerateSignature(KeyPair keyPair, string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            RSAParameters privateKeyParams = ConvertToRSAParameters(keyPair.Parameters);
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(privateKeyParams);
+                return rsa.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+
+        private RSAParameters ConvertToRSAParameters(Dictionary<string, byte[]> parameters)
+        {
+            return new RSAParameters
+            {
+                D = parameters["D"],
+                P = parameters["P"],
+                Q = parameters["Q"],
+                Modulus = parameters["Modulus"],
+                DQ = parameters["DQ"],
+                DP = parameters["DP"],
+                Exponent = parameters["Exponent"],
+                InverseQ = parameters["InverseQ"]
+            };
+        }
+
+        private void CompressAndSaveFiles(byte[] signature, string message, string publicKey)
+        {
+            try
+            {
+                // Guardar la firma, el mensaje y la llave publica en un archivo
+                File.WriteAllBytes("signature.txt", signature);
+                File.WriteAllText("message.txt", message);
+                File.WriteAllText("publicKey.txt", publicKey);
+
+                // Crear un nombre de archivo ZIP único
+                string zipFileName = $"firma_{GetUniqueZipCounter()}.zip";
+
+                // Crear un archivo ZIP para comprimir los archivos
+                using (ZipArchive zip = ZipFile.Open(zipFileName, ZipArchiveMode.Create))
+                {
+                    zip.CreateEntryFromFile("signature.txt", "signature.txt");
+                    zip.CreateEntryFromFile("message.txt", "message.txt");
+                    zip.CreateEntryFromFile("publicKey.txt", "publicKey.txt");
+                }
+
+                // Eliminar los archivos temporales
+                File.Delete("signature.txt");
+                File.Delete("message.txt");
+                File.Delete("publicKey.txt");
+
+                Console.WriteLine($"\nFirma, mensaje y llave pública comprimidos en {zipFileName}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al comprimir y guardar los archivos: {ex.Message}\n");
+            }
+        }
+        private int GetUniqueZipCounter()
+        {
+            int counter = zipCounter;
+            while (File.Exists($"firma_{counter}.zip"))
+            {
+                counter++;
+            }
+            zipCounter = counter + 1;
+            return counter;
         }
     }
 }
